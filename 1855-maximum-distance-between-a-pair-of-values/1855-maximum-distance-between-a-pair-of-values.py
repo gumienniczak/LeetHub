483ce3b31e2a09c928b0b928ad38a4a4e0b74b4a
gumienniczak <52624208+gumienniczak@users.noreply.github.com>
2025-07-13 21:34:04 +0200

    Time: 510 ms (10%), Space: 23.5 MB (86%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
@@ -5,15 +5,13 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: int
         """
-        def check(num1, num2):
-            return num1 <= num2
         ans = 0
         for i in range(len(nums1)):
             left = i
             right = len(nums2) - 1
             while left <= right:
                 mid = (right + left) // 2
-                if check(nums1[i], nums2[mid]):
+                if nums1[i] <= nums2[mid]:
                     left = mid + 1
                 else:
                     right = mid - 1
