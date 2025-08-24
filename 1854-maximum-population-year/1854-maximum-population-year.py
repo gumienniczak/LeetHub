21a909db15516b89debcbb99a44439ed3c1423ef
gumienniczak <52624208+gumienniczak@users.noreply.github.com>
2025-08-24 18:03:41 +0200

    Time: 29 ms (5.41%), Space: 12.4 MB (56.76%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maximumPopulation(self, logs):
+        """
+        :type logs: List[List[int]]
+        :rtype: int
+        """
+        max_pop = 0
+        cur_pop = 0
+        result_year = None
+        events = []
+
+        for b, d in logs:
+            events.append((b, 1))
+            events.append((d, -1))
+        
+        events.sort(key=lambda x: (x[0], x[1]))
+        print(events)
+        for year, change in events:
+            cur_pop += change
+            if cur_pop > max_pop:
+                max_pop = cur_pop
+                result_year = year
+        
+        return result_year
