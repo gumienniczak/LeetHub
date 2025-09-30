cb8092c91c936086fa37ee1cd926f864a28b19e3
gumienniczak <52624208+gumienniczak@users.noreply.github.com>
2025-09-30 14:19:34 +0200

    Time: 231 ms (20.41%), Space: 15.6 MB (18.24%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Solution(object):
     def characterReplacement(self, s, k):
         """
@@ -8,12 +9,13 @@ class Solution(object):
         N = len(s)
         left = 0
         ans = 0
-        counter = [0] * 26
+        counter = defaultdict(int)
         for right in range(N):
-            counter[ord(s[right]) - 65] += 1
-            while right - left + 1 - max(counter) > k:
-                counter[ord(s[left]) - 65] -= 1
+            counter[s[right]] += 1
+            while right - left + 1 - max(counter.values()) > k:
+                counter[s[left]] -= 1
+                if counter[s[left]] == 0:
+                    del counter[s[left]]
                 left += 1
             ans = max(ans, right - left + 1)
-        
         return ans
