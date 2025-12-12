33142743b5d9cd03f4448acd7c49524ec547a461
gumienniczak <52624208+gumienniczak@users.noreply.github.com>
2025-12-12 16:22:15 +0100

    Time: 134 ms (20%), Space: 12.6 MB (30%) - LeetHub

---
+++ 3433-count-mentions-per-user/3433-count-mentions-per-user.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def countMentions(self, numberOfUsers, events):
+        """
+        :type numberOfUsers: int
+        :type events: List[List[str]]
+        :rtype: List[int]
+        """
+        mentions = [0] * numberOfUsers
+        users_online = [1] * numberOfUsers
+        timestamps = [0] * numberOfUsers
+
+        events.sort(key = lambda x: x[0], reverse=True)
+        events.sort(key = lambda x: int(x[1]))
+    
+
+        for event in events:
+            tp = event[0] 
+            ts = int(event[1])
+            who = event[2] 
+            if tp == 'OFFLINE':
+                timestamps[int(who)] = ts
+                users_online[int(who)] = 0
+            
+            for idx in range(numberOfUsers):
+                if timestamps[idx] + 60 <= ts:
+                    users_online[idx] = 1
+            
+            if tp == 'MESSAGE':
+                if who == 'ALL':
+                    for idx in range(numberOfUsers):
+                        mentions[idx] += 1
+                elif who == 'HERE':
+                    for idx in range(numberOfUsers):
+                        if users_online[idx]:
+                            mentions[idx] += 1
+                else:
+                    for user in who.split():
+                        mentions[int(user[2:])] += 1
+
+        return mentions
+            
