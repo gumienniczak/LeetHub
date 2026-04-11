0ff8c7249beb2823897305d5230f4c06ef197c56
gumienniczak <52624208+gumienniczak@users.noreply.github.com>
2026-04-11 20:51:40 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -7,14 +7,10 @@ class Solution:
                 ans.append(cur)
                 return
             if left_count < n:
-                cur += "("
-                generate(cur, left_count+1, right_count)
-                cur = cur[:-1]
+                generate(cur + "(", left_count+1, right_count)
             
             if right_count < left_count:
-                cur += ")"
-                generate(cur, left_count, right_count+1)
-                cur = cur[:-1]
+                generate(cur + ")", left_count, right_count+1)
             
         generate("", 0, 0)
         return ans
